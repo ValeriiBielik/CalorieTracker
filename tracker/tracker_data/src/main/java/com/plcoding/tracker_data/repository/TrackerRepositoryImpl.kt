929666f39b048693976ff32b80ba3b40e3568b7a
Valerii <bielik.my@gmail.com>
2023-07-09 13:30:43 +0300

    [fix] - filter out wrong nutriment items from api

--- tracker/tracker_data/src/main/java/com/plcoding/tracker_data/repository/TrackerRepositoryImpl.kt
+++ tracker/tracker_data/src/main/java/com/plcoding/tracker_data/repository/TrackerRepositoryImpl.kt
@@ -19,7 +19,18 @@ class TrackerRepositoryImpl(
     override suspend fun searchFood(query: String, page: Int, pageSize: Int): Result<List<TrackableFood>> {
         return try {
             val searchDto = api.searchFood(query, page, pageSize)
-            Result.success(searchDto.products.mapNotNull { it.toTrackableFood() })
+            // todo in this case we have a small amount of items, think about solution. Also we have a problem of timeouts, i suppose due to api restrictions
+            Result.success(searchDto.products
+                .filter {
+                    val calculatedCalories =
+                        it.nutriments.carbohydrates100g * 4f +
+                                it.nutriments.proteins100g * 4f +
+                                it.nutriments.fat100g * 9f
+                    val lowerBound = calculatedCalories * 0.99f
+                    val upperBound = calculatedCalories * 1.01f
+                    it.nutriments.energyKcal100g in (lowerBound..upperBound)
+                }
+                .mapNotNull { it.toTrackableFood() })
         } catch (e: Exception) {
             e.printStackTrace()
             Result.failure(e)
